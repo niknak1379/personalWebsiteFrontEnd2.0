449548134386a27ed020471de97b8cd5485e7970
niknak1379 <nikanostovan@gmail.com>
2025-10-26 15:54:50 -0700

    s

--- src/index.js
+++ src/index.js
@@ -1,23 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router';
-import { AuthProvider } from './Context/authProvider';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router";
+import { AuthProvider } from "./Context/authProvider.js";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  
-
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>      
-    </BrowserRouter>
- 
-  
-  
+	<BrowserRouter>
+		<AuthProvider>
+			<App />
+		</AuthProvider>
+	</BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
