2a82177888caae1d532d8e3ce03c22ce7be42b57
niknak1379 <nikanostovan@gmail.com>
2026-01-28 14:49:36 -0800

    added backend endpoint change for diff environments

--- src/utils/endpoints.js
+++ src/utils/endpoints.js
@@ -1,5 +1,12 @@
 // This is basically my .env file, didnt really need to add the whole module tbh
 // and i dont have any more .env variables.
 
-export const ServerEndpoint = "http://localhost:8080";
-//export const ServerEndpoint = "https://api.nikanostovan.dev";
+export var ServerEndpoint
+let env = process.env.DEP_ENV
+if (env === "test") {
+  ServerEndpoint = "https://api.nikanostovan.dev/123345"
+} else if (env === "local") {
+  ServerEndpoint = "http://localhost:8080"
+} else if (env === "prod") {
+  ServerEndpoint = "https://api.nikanostovan.dev"
+}
