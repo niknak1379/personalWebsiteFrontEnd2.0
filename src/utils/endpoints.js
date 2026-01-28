917da86db8d329682b7cd3b5d8cf5dc4f7a9ac94
niknak1379 <nikanostovan@gmail.com>
2026-01-28 15:03:18 -0800

    messy

--- src/utils/endpoints.js
+++ src/utils/endpoints.js
@@ -1,6 +1,8 @@
 // This is basically my .env file, didnt really need to add the whole module tbh
 // and i dont have any more .env variables.
+import dotenv from 'dotenv'
 
+dotenv.config()
 var ServerEndpoint
 let env = process.env.DEP_ENV
 console.log("env", env)
